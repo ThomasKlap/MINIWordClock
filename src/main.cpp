606356ad9cc4439cf86a616e17d819035a420916
ThomsKlap <froebel@gmx.net>
2024-06-09 19:47:16 +0200

    Anpassungen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
-#include "MATRIX7219.h"
+#include <MATRIX7219.h>
+#include <WiFiManager.h>
+#include <time.h>
+#include <matrix.h>
 
 //
 //    FILE: MINI WordClock
@@ -13,19 +16,48 @@ uint8_t clockPin  = D4;
 uint8_t count     = 1;
 MATRIX7219 mx(dataPin, selectPin, clockPin, count);
 
+int lastMilis;
+int temp;
+unsigned long CycleMillis = 0; // speichert den Zeitpunkt des Letzten Zyklus
+const char *NTP_SERVER = "de.pool.ntp.org";
+const char *TZ_INFO = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00";
+
+char wochentage[7][12] = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
+String monat[12] = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+
+time_t now;
+tm tm;
+
+
+
+
 void setup()
 {
   Serial.begin(115200);
-  Serial.println(__FILE__);
-  Serial.print("MATRIX7219_LIB_VERSION: ");
-  Serial.println(MATRIX7219_LIB_VERSION);
-  Serial.println();
+  
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("MiniWordClock", "123456789"); // password protected ap
+  if (!res)
+  {
+    Serial.println("Failed to connect");
+    // ESP.restart();
+  }
+  else
+  {
+    // if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+  }
 
+  configTime(0, 0, NTP_SERVER);
+  setenv("TZ", TZ_INFO, 1);
 
   mx.begin();
   mx.clear();
   mx.setBrightness(10);
   mx.setReverse(true);
+
+
  }
 
 
@@ -39,7 +71,12 @@ void loop()
     mx.setRow(6, 6, 1);
     mx.setRow(7, 7, 1);
     mx.setRow(8, 8, 1);
-    Serial.println("BILD ");
+   Serial.print(tm.tm_hour);
+   Serial.print(" : ");
+   Serial.print(tm.tm_min);
+   Serial.print(" : ");
+   Serial.print(tm.tm_sec);
+   Serial.println("");
     delay(100);
     mx.displayOff();
     delay(100);
